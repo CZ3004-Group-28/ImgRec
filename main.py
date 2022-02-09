ef0d0945d10fb5b939beec19188abe8ec9b8c32a
ianpye <ianpangye@gmail.com>
2022-02-10 01:42:35 +0800

    Added stitch_image function and offline model loading

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-import cv2
 import torch
-import os
 from PIL import Image
-import time
-import pandas
+from imutils import paths
 
+
+# Initializes the model at the start, run model = load_model
 def load_model():
-    model = torch.hub.load('ultralytics/yolov5', 'custom', path='yolov5s_best.pt')
+    model = torch.hub.load('./', 'custom', path='yolov5s_best.pt', source='local')
     return model
 
+# Takes in the image from the 'test' folder, and outputs the predicted label - sample at the end
+# Images with predicted bounding boxes are saved in the runs folder
 def predict_image(image,model):
     img = Image.open('test/' + image)
     results = model(img)
+    results.save('runs')
     pred_list = results.pandas().xyxy[0]['name'].to_numpy()
     pred = 'NA'
     for i in pred_list:
@@ -19,6 +21,29 @@ def predict_image(image,model):
             pred = i
     return pred
 
+# Stitches the previously predicted images in the folder together and saves it into runs/stitched folder
+# This function can be called by itself
+def stitch_image():
+    imgFolder = 'runs'
+    newPath = 'runs/stitched.jpg'
+    imgPath = list(paths.list_images(imgFolder))
+    images = [Image.open(x) for x in imgPath]
+    width, height = zip(*(i.size for i in images))
+    total_width = sum(width)
+    max_height = max(height)
+    stitchedImg = Image.new('RGB', (total_width, max_height))
+    x_offset = 0
+    for im in images:
+        stitchedImg.paste(im, (x_offset,0))
+        x_offset += im.size[0]
+    stitchedImg.save(newPath)
+
+## Load
 # model = load_model()
-# image = '1_5.jpg'
-# print(predict_image(image,model))
+
+## Predict
+# image = 'filename.jpg'
+# print(predict_image(image, model))
+
+
+
