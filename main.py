b7127e7c9552556928ce73eb0b031dc827c8f777
ianpye <ianpangye@gmail.com>
2022-02-10 00:32:46 +0800

    Added comments

--- main.py
+++ main.py
@@ -9,11 +9,7 @@ def load_model():
     model = torch.hub.load('ultralytics/yolov5', 'custom', path='yolov5s_best.pt')
     return model
 
-model = load_model()
-
-image = '1_5.jpg'
-
-def predict_image(image):
+def predict_image(image,model):
     img = Image.open('test/' + image)
     results = model(img)
     pred_list = results.pandas().xyxy[0]['name'].to_numpy()
@@ -22,4 +18,7 @@ def predict_image(image):
         if i != 'Bullseye':
             pred = i
     return pred
-print(predict_image(image))
+
+# model = load_model()
+# image = '1_5.jpg'
+# print(predict_image(image,model))
